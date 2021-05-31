a855be768a9c4f00b980977988f8207306e6cfa7
joannsiciliano <joannsiciliano@gmail.com>
2021-05-31 11:27:55 -0600

    user table added with username and password digest

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+  def create
+    @user=User.create(user_params)
+    render json: @user, status: :created
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
